b28924cbe7506f7ac31739804d319b1fa82a8f99
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-06 16:10:30 +0100

    Adat felvétel konzolról

--- src/main/java/hu/petrik/adatbaziskonzolos/Main.java
+++ src/main/java/hu/petrik/adatbaziskonzolos/Main.java
@@ -2,17 +2,48 @@ package hu.petrik.adatbaziskonzolos;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+   static DogDB dogDB;
     public static void main(String[] args) {
         try {
-            DogDB dogDB = new DogDB();
-            List<Dog> dogs = dogDB.getDogs();
-            for (Dog dog : dogs) {
-                System.out.println(dog);
-            }
+            dogDB = new DogDB();
+           readDogsFromConsole();
+            listDogs();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void listDogs() throws SQLException {
+        List<Dog> dogs = dogDB.getDogs();
+        for (Dog dog : dogs) {
+            System.out.println(dog);
+        }
+        System.out.println();
+    }
+
+    private static void readDogsFromConsole() {
+        System.out.print("Adja meg, hogy hány kutyát szeretne felvenni: ");
+        Scanner sc = new Scanner(System.in);
+        try {
+            int count = Integer.parseInt(sc.nextLine());
+            for (int i = 0; i < count; i++) {
+                System.out.print("Adja meg a kutya nevét: ");
+                String name = sc.nextLine();
+                System.out.print("Adja meg a kutya életkorát: ");
+                int age = Integer.parseInt(sc.nextLine());
+                System.out.print("Adja meg a kutya fajtáját: ");
+                String breed = sc.nextLine();
+                Dog dog = new Dog(name, age, breed);
+                dogDB.createDog(dog);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Nem számot adott meg a felvétel megszakad!");
+        } catch (SQLException e) {
+            System.out.println("Hiba történt a kutya felvétele során: ");
+            e.printStackTrace(System.out);
         }
     }
 }
